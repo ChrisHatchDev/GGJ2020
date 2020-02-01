0447ea1b2f4711c10c44aeb406141addfc3a87f8
Bradley Wascher <BradleyWascher@gmail.com>
2020-02-01 10:59:14 -0500

    BW prototype tests
    Getting prototype items in for character movement

---
+++ Assets/PrototypeTrash/BW_StayUpright.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BW_StayUpright : MonoBehaviour
+{
+    private GameObject targetObject;
+    public 
+
+     void Start()
+    {
+        targetObject = gameObject; 
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Smoothly rotates towards target 
+        var targetPoint = targetObject.transform.position;
+        var targetRotation = Quaternion.LookRotation(targetPoint - transform.position, Vector3.up);
+        transform.rotation = targetRotation;
+    }
+}
