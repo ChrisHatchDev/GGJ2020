607442f51441f215557b85faaff2f8f2a5444ee9
zekio309 <zekio309@yahoo.com>
2020-02-01 21:32:38 -0500

    Created MainMenu Scene
    Buttons currently load as follows;
    
    Play = SampleScene
    Restart = MainMenu
    Exit = Close Application

---
+++ Assets/Code/MainMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+public void OnPlayButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+public void OnRestartButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+public void OnExitButton()
+    {
+        Application.Quit();
+    }
+
+}
