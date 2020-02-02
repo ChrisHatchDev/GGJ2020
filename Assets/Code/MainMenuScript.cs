bbeded775a6b5ade81352fcca0d1b68415675e11
zekio309 <zekio309@yahoo.com>
2020-02-02 03:01:12 -0500

    Added Pause Menu
    Pause menu is functioning like it should as well as the main menu loading you currently into the tech demo. can change the level to load into after it is finalized.

--- Assets/Code/MainMenuScript.cs
+++ Assets/Code/MainMenuScript.cs
@@ -1,24 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
 
-public void OnPlayButton()
+    public Canvas pauseMenu;
+    public GameObject pC;
+    public bool pauseActive;
+
+    void Awake()
     {
-        SceneManager.LoadScene("SampleScene");
+        pauseActive = false;
+        
     }
 
-public void OnRestartButton()
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("TechPlayground");
+    }
+
+    public void OnRestartButton()
     {
         SceneManager.LoadScene("MainMenu");
     }
 
-public void OnExitButton()
+    public void OnExitButton()
     {
         Application.Quit();
     }
 
+    public void WakeUpMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Debug.Log("L key was pressed.");
+
+            if (pauseActive == true)
+            {
+                pC.SetActive(false);
+                //Debug.Log("I am NOT Active");
+                pauseActive = false;
+            }
+
+            else if (pauseActive == false)
+            {
+                pC.SetActive(true);
+                //Debug.Log("I am Active");
+                pauseActive = true;
+            }
+
+        }
+    }
+
+    void Update()
+    {
+        WakeUpMenu();
+    }
 }
